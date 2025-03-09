e2f96464b9bda3fea4df63e467b0de4523ccd966
jawad-29 <jawadahmad029@outlook.com>
2025-03-09 23:20:13 +0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Kubernetes!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
